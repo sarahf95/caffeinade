00e8c2f3243dacbcb31494742e97eee73093cadc
sarahfeldmann <sarahf95@gmail.com>
2018-05-07 21:48:41 -0700

    add className

--- src/Footer.js
+++ src/Footer.js
@@ -8,15 +8,15 @@ import venmo from "./img/venmo.png";
 class Footer extends Component {
   render() {
     return (
-      <div class="footer">
+      <div className="footer">
         <a href="https://www.facebook.com/caffeinade/" target="_blank">
-          <img class=" social " src={facebook} alt="facebook" />
+          <img className=" social " src={facebook} alt="facebook" />
         </a>
         <a href="https://www.instagram.com/drinkcaffeinade " target="_blank ">
-          <img class="social " src={instagram} alt="instagram " />
+          <img className="social " src={instagram} alt="instagram " />
         </a>
         <a target="_blank " href="https://venmo.com/caffeinade ">
-          <img src={venmo} class="social venmo " alt="venmo " />
+          <img src={venmo} className="social venmo " alt="venmo " />
         </a>
       </div>
     );
