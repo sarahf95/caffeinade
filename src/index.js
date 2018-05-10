1d55915365f6de99685dfc1eef743d0d714ed6c6
sarahfeldmann <sarahf95@gmail.com>
2018-05-10 02:12:10 -0700

    fix build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,9 +8,11 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
     <Router>
         <div className="App">
-            <Route exact path="/" component={App} />{' '}
-            <Route exact path="/about" component={App} />{' '}
-            <Route exact path="/events" component={App} />{' '}
+            <Switch>
+                <Route exact path="/" component={App} />{' '}
+                <Route exact path="/about" component={App} />{' '}
+                <Route exact path="/events" component={App} />{' '}
+            </Switch>
         </div>
     </Router>,
     document.getElementById('root')
