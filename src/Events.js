3dc1fd34a2c16c542281639d5952d1611f66d366
sarahfeldmann <sarahf95@gmail.com>
2018-05-14 20:59:37 -0700

    add street fair

--- src/Events.js
+++ src/Events.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './css/events.css';
 import carnival from './img/carnival.png';
-import cherryblossom from './img/cherryblossom.png';
+import streetfair from './img/streetfair.png';
 
 class Events extends Component {
     render() {
@@ -14,31 +14,33 @@ class Events extends Component {
                         <li>
                             <img
                                 className="icon"
-                                src={cherryblossom}
-                                alt="balloon"
-                            />
-                            <div className="content-wrapper">
-                                <div className="event-name">Weekly Tabling</div>
-                                <div className="date">
-                                    May 17th | 11:00 - 2:00
-                                </div>
-                                <div className="loc">UW Quad </div>
-                            </div>
-                        </li>
-                        <li>
-                            <img
-                                className="icon"
                                 src={carnival}
                                 alt="balloon"
                             />
                             <div className="content-wrapper">
                                 <div className="event-name">
-                                    Junior Panhellenic Carnival
+                                    Junior Panhellenic Spring Carnival
                                 </div>
                                 <div className="date">
                                     May 18th | 2:00 - 4:00
                                 </div>
                                 <div className="loc">Red Square </div>
+                            </div>
+                        </li>
+                        <li>
+                            <img
+                                className="icon street-fair"
+                                src={streetfair}
+                                alt="balloon"
+                            />
+                            <div className="content-wrapper">
+                                <div className="event-name">
+                                    University District Street Fair
+                                </div>
+                                <div className="date">
+                                    May 19th | 10:00am - 7:00pm
+                                </div>
+                                <div className="loc">The Ave</div>
                             </div>
                         </li>
                     </ul>
